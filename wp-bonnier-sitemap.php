1835f570b9eed60cf8f2c08b9071e4f26a49c764
materialsmoke <101429685+materialsmoke@users.noreply.github.com>
2022-08-02 12:05:04 +0200

    add-new-release-1.6.0

--- wp-bonnier-sitemap.php
+++ wp-bonnier-sitemap.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP Bonnier Sitemap
- * Version: 1.5.5
+ * Version: 1.6.0
  * Plugin URI: https://github.com/BenjaminMedia/wp-bonnier-sitemap
  * Description: This plugin creates sitemaps with support for Polylang
  * Author: Bonnier Publications
