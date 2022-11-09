becaaa601fd0f036de1f0611000b34b07258d377
materialsmoke <101429685+materialsmoke@users.noreply.github.com>
2022-11-09 13:06:40 +0100

    add-editors-in-the-sitemap

--- src/Commands/GenerateCommand.php
+++ src/Commands/GenerateCommand.php
@@ -141,7 +141,8 @@ class GenerateCommand extends \WP_CLI_Command
         $authorsProgress = make_progress_bar(sprintf('Generating %s sitemap entries for authors...', number_format($authorsCount)), $authorsCount);
         foreach ($authors as $author) {
             $hasAuthorRole = in_array('author', $author->roles);
-            if ($hasAuthorRole) {
+            $hasEditorRole = in_array('editor', $author->roles);
+            if ($hasAuthorRole || $hasEditorRole) {
                 $allowInSitemap = apply_filters(WpBonnierSitemap::FILTER_ALLOW_USER_IN_SITEMAP, true, $author->ID, $author);
                 foreach (LocaleHelper::getLanguages() as $locale) {
                     $countUserPosts = Utils::countUserPosts($author, $locale);
