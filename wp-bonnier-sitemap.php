18e97ef8660ed399af338b159aa0993e17b801c4
materialsmoke <101429685+materialsmoke@users.noreply.github.com>
2022-11-16 14:40:20 +0100

    compatible with php7.4

--- wp-bonnier-sitemap.php
+++ wp-bonnier-sitemap.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP Bonnier Sitemap
- * Version: 1.7.1
+ * Version: 1.7.2
  * Plugin URI: https://github.com/BenjaminMedia/wp-bonnier-sitemap
  * Description: This plugin creates sitemaps with support for Polylang
  * Author: Bonnier Publications
